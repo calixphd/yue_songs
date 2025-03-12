79d8bc3309c1704f85ecc9969f85e57ec5f0ecdd
CHINONSO ONAH <83706504+calixphd@users.noreply.github.com>
2025-03-12 23:34:03 +0100

    Add files via upload

---
+++ setup_conda.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+import subprocess
+
+CONDA_URL = "https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh"
+if __name__ == "__main__":
+    subprocess.run(f"curl -O {CONDA_URL}", shell=True)
+    subprocess.run(f"bash {CONDA_URL.split('/')[-1]} -b", shell=True)
+    subprocess.run("source ~/.bashrc", shell=True)
